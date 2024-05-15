a9a24f29befe71df8e812318817d2420c4254fd1
bkaoukine <b.koukin@gmail.com>
2024-05-15 21:33:07 +0100

    2nd deployement

--- app/component/about.tsx
+++ app/component/about.tsx
@@ -10,9 +10,9 @@ function About()
                         <strong>About</strong>
                     </h1>
 
-                    <h1 className="text-[#1D2130] text-lg mb-12"> Welcome to our task management web app! We're on a mission to simplify task management for individuals and teams without compromising on privacy or usability.
+                    <h1 className="text-[#1D2130] text-lg mb-12"> Welcome to our task management web app! We&apos;re on a mission to simplify task management for individuals and teams without compromising on privacy or usability.
 
-                    At <strong>TASKLIGHT</strong>, we believe in the power of simplicity. Our platform is designed to streamline your workflow, whether you're a busy professional, a student with deadlines, or someone managing multiple projects.</h1>
+                    At <strong>TASKLIGHT</strong>, we believe in the power of simplicity. Our platform is designed to streamline your workflow, whether you&apos;re a busy professional, a student with deadlines, or someone managing multiple projects.</h1>
 
                     <ul className="text-[#1D2130] space-y-4">
                     
@@ -21,7 +21,7 @@ function About()
                         </li>
                         <p>
                         Your privacy is our top priority. Unlike other task management tools,
-                         we don't require any personal information to register. With just a username and email,
+                         we don&apos;t require any personal information to register. With just a username and email,
                           you can create an account and start using our platform immediately. Rest assured, we do not collect any user data. Your tasks remain private and secure at all times.
                         </p>
 
@@ -30,8 +30,8 @@ function About()
                         </li>
                         <p>
                         We believe that staying organized should be effortless and accessible to everyone.
-                         That's why our web app is completely free to use, with no subscriptions or hidden costs.
-                          Whether you're managing personal projects or collaborating with a team, our platform is built to adapt to your needs.
+                         That&apos;s why our web app is completely free to use, with no subscriptions or hidden costs.
+                          Whether you&apos;re managing personal projects or collaborating with a team, our platform is built to adapt to your needs.
 
                         </p>
                     
